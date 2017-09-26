1f79b3beec785c8efe0d8754db9c69afd9eeee86
ceylan9218 <ceylan.haze9218@outlook.com>
2017-09-26 11:10:06 +0200

    Update API

--- server.js
+++ server.js
@@ -2,32 +2,36 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var routes = require('./routes');
-var user = require('./server/user');
-var port = process.env.PORT || 3000;
-var app = express()
+var http = require('http');
+var app = express();
+var db = require('./config/db');
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json({
-  type: 'application/*'
-}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/agentcloud', { useMongoClient: true });
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  app.listen(port, () => {
-    console.log('listening on 3000')
-  })
-});
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-app.post('/api/user', user.createAccount)
-app.put('/api/user/:userId', user.updateAccount)
-app.get('/api/user', user.getAllAccounts)
-app.get('/api/user/:userId', user.getAccount)
-app.delete('/api/user/:userId', user.deleteAccount)
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(204);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
+// API location
+require('./routes')(app);
+
+
+// Set Port
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port, () => console.log('Running on localhost:'+port));
